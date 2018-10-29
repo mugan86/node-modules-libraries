183e178831671ee438e5a95adba7dc73a4277a9b
Anartz Mugika <mugan86@gmail.com>
2018-10-29 14:26:22 +0100

    Create node module package and define functions to use in module in index.js file

---
+++ project0/index.js
@@ -0,0 +1,41 @@
+module.exports = {
+    /**
+     *  Addition
+     * @example 6 + 8 = 14
+     * 
+     * @param {*} number1 First number to use in addition
+     * @param {*} number2 Second number to use in addition
+     * @returns {number} addition result in function
+     */
+    addition: function (number1, number2) {
+        return number1 + number2;
+    },
+    /**
+     * @example 89 - 8 = 81
+     * @param {*} number1 
+     * @param {*} number2 
+     * @returns {number} subtraction result in function
+     */
+    subtraction: function (number1, number2) {
+        return number1 - number2;
+    },
+    /**
+     * @example 201 * 10 = 2010
+     * @param {*} number1 
+     * @param {*} number2 
+     * @returns {number} multiplication result in function
+     */
+    multiplication: function (number1, number2) {
+        return number1 * number2;
+    },
+    /**
+     * @example 50 / 2 = 25
+     * @param {*} number1 
+     * @param {*} number2 
+     * @returns {number} division result in function
+     */
+    division: function (number1, number2) {
+        return number1 / number2;
+    }
+
+}
